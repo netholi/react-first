63b501b2db50989b4773fb938a2995a6c6379d9e
Anand Janardhanan <ajkadakkal@gmail.com>
2026-05-16 13:44:57 +0530

    using useEffect - not recomended

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import Card from "./component/Card";
-import indianStates from "./component/States";
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const data = {
+    Kerala: "Thiruvananthapuram",
+    Karnataka: "Bengaluru",
+    Tamilnadu: "Chenni",
+  };
+
+  const [selectedState, setSelectedState] = useState("Kerala");
+  const [selectedCapital, setSelectedCapital] = useState("Thiruvananthapuram");
+
+  const selectState = (e) => {
+    setSelectedState(e.target.value);
+  };
+  const changeCapital = () => {
+    setSelectedCapital(data[selectedState]);
+  };
+  useEffect(changeCapital, [selectedState]);
+
   return (
     <>
-      <h1>hello states </h1>
-      {indianStates.map((item) => (
-        <Card key={item.state} data={item} />
-      ))}
+      <select onChange={selectState}>
+        <option value="Kerala"> Kerala </option>
+        <option value="Karnataka"> Karnataka </option>
+        <option value="Tamilnadu"> Tamilnadu </option>
+      </select>
+
+      <p>
+        {" "}
+        Selected state is : {selectedState} capital is : {selectedCapital}
+      </p>
     </>
   );
+}
+
+{
+  /*
+    viwsa"  - add quote to a word
+  sd" - delete quote around word
+  sr"{ - replace quote with {
+
+
+    */
 }
