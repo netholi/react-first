dc04f36692b837e52ddf673e81aeea88df506fa1
Anand Janardhanan <ajkadakkal@gmail.com>
2026-05-16 15:39:08 +0530

    example using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import Card from "./component/Card";
-import indianStates from "./component/States";
+import { useRef, useState } from "react";
+
 export default function App() {
+  const txtRef = useRef();
+  const [txt, setTxt] = useState("");
   return (
-    <>
-      <h1>hello states </h1>
-      {indianStates.map((item) => (
-        <Card key={item.state} data={item} />
-      ))}
-    </>
+    <div>
+      <input ref={txtRef} type="text" />
+      <button
+        onClick={() => {
+          console.log(txtRef.current.value);
+          setTxt(txtRef.current.value);
+        }}
+      >
+        {" "}
+        getText{" "}
+      </button>
+      <p> input text = {txt} </p>
+    </div>
   );
+}
+
+{
+  /*
+    viwsa"  - add quote to a word
+  sd" - delete quote around word
+  sr"{ - replace quote with {
+
+
+    */
 }
